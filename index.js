3eb24b43a1212a68a62a3da707b0a7b232304983
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-21 19:59:38 -0400

    add get request that returns index file with table data

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const scores = require('./scores')
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  response.render('index', { scores })
+})
+
+app.all('*'), (request, response) => {
+  return request.sendStatus(404)
+}
+
+app.listen(1400, () => {
+  console.log('Listening on 1400...') // eslint-disable-line no-console
+})
